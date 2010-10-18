e0bbb2d4ce67e35c0ccf4f36bd41fbf68a841d2f
Ole Streicher <github@liska.ath.cx>
2010-10-18 10:00:56 +0200

    Switch OFF terminal messages by default. Since they are deprecated, the user should switch them on manually.

--- cpl/__init__.py
+++ cpl/__init__.py
@@ -8,5 +8,6 @@ from frames import FrameConfig, Result, CplError
 from log import msg
 
 msg.domain = os.path.basename(sys.argv[0])
+msg.level = msg.OFF
 
 Recipe.dir = '/work1/oles/Projects/2009/Muse/Pipeline/lib/muse/'
