a3724d397897c7543e365083e99b0d3598141afc
Ole Streicher <github@liska.ath.cx>
2011-03-07 13:11:35 +0100

    Replace distribution name in changelog by mkdebian script

--- mkdebian.py
+++ mkdebian.py
@@ -3,36 +3,57 @@ import os
 import shutil
 import subprocess
 import tarfile
-#from cpl.version import __version__ as cpl_version
-cpl_version = '0.3.2'
 
-basedir = 'debian_build'
+vline = open(os.path.join('debian', 'changelog')).readline()
+cpl_version = vline.split('(', 1)[1].split('-')[0]
+cpl_release = vline.split('(', 1)[1].split('-')[1].split(')')[0]
+
 module = 'cpl'
+files = fnmatch.filter(os.listdir(module), '*.py') + ['CPL_recipe.c']
 
-shutil.rmtree(basedir, ignore_errors= True)
-os.mkdir(basedir)
-os.mkdir(os.path.join(basedir, module))
-os.mkdir(os.path.join(basedir, 'debian'))
+# 1. build common tar file
 #tar = tarfile.open('python-cpl-%s.tar.gz' % cpl_version, mode='w:gz')
 tar = tarfile.open('python-cpl.tar.gz', mode='w:gz')
-
-shutil.copy('setup.py', basedir)
 tar.add('setup.py')
-shutil.copy('README', basedir)
 tar.add('README')
-
-files = fnmatch.filter(os.listdir(module), '*.py') + ['CPL_recipe.c']
 for fname in files:
-    shutil.copy(os.path.join(module, fname), os.path.join(basedir, module))
     tar.add(os.path.join(module, fname))
-
 tar.close()
 
-for fname in ['compat', 'control', 'copyright', 'docs', 'rules', 'changelog' ] :
-    shutil.copy(os.path.join('debian', fname), os.path.join(basedir, 'debian'))
+# 2. Build distribution dependent files
+for distrib in [ 'lucid', 'maverick' ]:
+    basedir = os.path.abspath('debian_build_%s' % distrib)
 
-os.chdir(basedir)
-subprocess.call(['dpkg-buildpackage', '-S'])
+    shutil.rmtree(basedir, ignore_errors= True)
+    os.mkdir(basedir)
+    os.mkdir(os.path.join(basedir, module))
+    os.mkdir(os.path.join(basedir, 'debian'))
 
-os.chdir('..')
-#subprocess.call(['dput', 'ppa:olebole/astro', 'python-cpl_%s-3_source.changes' % cpl_version])
+    shutil.copy('setup.py', basedir)
+    shutil.copy('README', basedir)
+
+    for fname in files:
+        shutil.copy(os.path.join(module, fname), os.path.join(basedir, module))
+
+    for fname in ['compat', 'control', 'copyright', 'docs', 'rules', ] :
+        shutil.copy(os.path.join('debian', fname), 
+                    os.path.join(basedir, 'debian'))
+
+    changelog_orig = open(os.path.join('debian', 'changelog'))
+    changelog_dist = open(os.path.join(basedir, 'debian', 'changelog'), 'w')
+    for line in changelog_orig:
+        changelog_dist.write(line.replace('$DISTRIB', distrib))
+    changelog_orig.close()
+    changelog_dist.close()
+
+    os.chdir(basedir)
+    subprocess.call(['dpkg-buildpackage', '-S'])
+
+    os.chdir('..')
+    subprocess.call([
+            'dput', 'ppa:olebole/astro', 
+            'python-cpl_%s-%s_source.changes' 
+            % (cpl_version, cpl_release.replace('$DISTRIB', distrib))
+            ])
+
+
