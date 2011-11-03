bfe254a652ac803d07d34d1a45e44d9a2ae75cc4
Ole Streicher <github@liska.ath.cx>
2011-11-03 11:29:47 +0100

    Correct tar file name, build by setup.py sdist

--- setup.py
+++ setup.py
@@ -18,10 +18,6 @@ long_description = "\n".join(description[2:])
 description = description[0]
 
 def create_version_file(cpl_version = cpl_version):
-    changelog = os.path.join('debian', 'changelog')
-    if os.path.exists(changelog):
-        vline = open(changelog).readline()
-        cpl_version = vline.split('(', 1)[1].split('-')[0]
     vfile = open(os.path.join('cpl', 'version.py'), 'w')
     vfile.write("version = %s\n" % repr(cpl_version))
     vfile.write("author = %s\n" % repr(author))
@@ -36,8 +32,9 @@ module1 = Extension('cpl.CPL_recipe',
                                   'gomp', 'mcheck' ],
                     sources = ['cpl/CPL_recipe.c'])
 
+
 create_version_file()
-setup (name = 'cpl',
+setup (name = 'python-cpl',
        version = cpl_version, author = author, author_email = email, 
        description = description, long_description = long_description,  
        license = license_, url = 'http://www.aip.de/~oles/python-cpl/',
