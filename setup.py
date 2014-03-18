1c6838f50570e6e80b7f22c69031a3cdcfad5cf9
Ole Streicher <debian@liska.ath.cx>
2014-03-18 10:21:01 +0100

    New release 0.6.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pkg_resources import require, DistributionNotFound
 author = 'Ole Streicher'
 email = 'python-cpl@liska.ath.cx'
 license_ = 'GPL'
-cpl_version = '0.6'
+cpl_version = '0.6.2'
 with open('README') as readme:
     description = readme.read().splitlines()
     long_description = "\n".join(description[2:])
