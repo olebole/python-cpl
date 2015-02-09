bdc0c5e1254e5e10b7440ba2eaf8fa478cb44cd5
Ole Streicher <ole@aip.de>
2015-02-09 15:46:55 +0100

    Allow more than one RECi entry in FITS file

--- cpl/dfs.py
+++ cpl/dfs.py
@@ -86,12 +86,14 @@ class ProcessingInfo(object):
           accompanied with the MD5 sum.
     '''
 
-    def __init__(self, source, datapaths = None):
+    def __init__(self, source, recno = 1, datapaths = None):
         '''
         :param source: Object pointing to the result file header
         :type source: :class:`str` or :class:`astropy.io.fits.HDUList`
                       or :class:`astropy.io.fits.PrimaryHDU` or 
                       :class:`astropy.io.fits.Header`
+        :param recno: Record number. Optional.
+        :type recno: :class:`int`
         :param datapaths: Dictionary with frame tags as keys and directory paths
                         as values to provide a full path for the raw and
                         calibration frames. Optional.
@@ -109,13 +111,13 @@ class ProcessingInfo(object):
             raise ValueError('Cannot assign type {0} to header'.format(
                     source.__class__.__name__))
         
-        self.name = header['HIERARCH ESO PRO REC1 ID']
+        self.name = header['HIERARCH ESO PRO REC{1} ID'.format(recno)]
         self.product = header['HIERARCH ESO PRO CATG']
         self.orig_filename = header['PIPEFILE']
         if datapaths and self.product in datapaths:
             self.orig_filename = os.path.join(datapaths[self.product], 
                                               self.orig_filename)
-        pipe_id = header.get('HIERARCH ESO PRO REC1 PIPE ID')
+        pipe_id = header.get('HIERARCH ESO PRO REC{1} PIPE ID'.format(recno))
         if pipe_id:
             self.pipeline, version = pipe_id.split('/')
             num_version = 0
@@ -125,22 +127,22 @@ class ProcessingInfo(object):
         else:
             self.pipeline =  None
             self.version = None
-        self.cpl_version = header.get('HIERARCH ESO PRO REC1 DRS ID')
+        self.cpl_version = header.get('HIERARCH ESO PRO REC{1} DRS ID'.format(recno))
         self.md5sum = header.get('DATAMD5')
         self.md5sums = {}
-        self.calib = _get_rec_keys(header, 'CAL', 'CATG', 'NAME', datapaths)
-        for cat, md5 in _get_rec_keys(header, 'CAL', 'CATG', 'DATAMD5').items():
+        self.calib = _get_rec_keys(header, recno, 'CAL', 'CATG', 'NAME', datapaths)
+        for cat, md5 in _get_rec_keys(header, recno, 'CAL', 'CATG', 'DATAMD5').items():
             if isinstance(md5, list):
                 for m, f in zip(md5, self.calib[cat]):
                     if m is not None: 
                         self.md5sums[f] = m
             elif md5 is not None:
                 self.md5sums[self.calib[cat]] = md5
-        raw = _get_rec_keys(header, 'RAW', 'CATG', 'NAME', datapaths)
+        raw = _get_rec_keys(header, recno, 'RAW', 'CATG', 'NAME', datapaths)
         if raw:
             self.tag = list(raw.keys())[0]
             self.raw = raw[self.tag]
-            md5 = _get_rec_keys(header, 'RAW', 'CATG', 'DATAMD5')[self.tag]
+            md5 = _get_rec_keys(header, recno, 'RAW', 'CATG', 'DATAMD5')[self.tag]
             if isinstance(md5, list):
                 for m, f in zip(md5, self.raw):
                     if m is not None: 
@@ -151,7 +153,7 @@ class ProcessingInfo(object):
             self.tag = None
             self.raw = None
             self.input = None
-        param = _get_rec_keys(header, 'PARAM', 'NAME', 'VALUE')
+        param = _get_rec_keys(header, recno, 'PARAM', 'NAME', 'VALUE')
         self.param = dict()
         for k,v in param.items():
             self.param[k] = _best_type(v)
@@ -222,7 +224,7 @@ class ProcessingInfo(object):
         '''
         print(str(self))
 
-def _get_rec_keys(header, key, name, value, datapaths = None):
+def _get_rec_keys(header, recno, key, name, value, datapaths = None):
     '''Get a dictionary of key/value pairs from the DFS section of the
     header.
 
@@ -230,6 +232,8 @@ def _get_rec_keys(header, key, name, value, datapaths = None):
                 parameters, 'RAW' for raw frames, and 'CAL' for 
                 calibration frames.
     :type key: :class:`str`
+    :param recno: Record number.
+    :type recno: :class:`int`
     :param name: Header keyword (last part) for the name of each key
     :type name: :class:`str`
     :param value: Header keyword (last part) for the value of each key
@@ -248,7 +252,7 @@ def _get_rec_keys(header, key, name, value, datapaths = None):
       
     is called with
 
-      _get_rec_keys('PARAM', 'NAME', 'VALUE')
+      _get_rec_keys(1, 'PARAM', 'NAME', 'VALUE')
 
     the returned dictionary will contain the keys
 
@@ -258,9 +262,9 @@ def _get_rec_keys(header, key, name, value, datapaths = None):
     res = dict()
     for i in range(1, 2**16):
         try:
-            prefix = 'HIERARCH ESO PRO REC1 %s%i' % (key, i)
-            k = header['%s %s' % (prefix, name)]
-            fn = header.get('%s %s' % (prefix, value))
+            prefix = 'HIERARCH ESO PRO REC{1} {2}{3}'.format(recno, key, i)
+            k = header['{1} {2}'.format(prefix, name)]
+            fn = header.get('{1} {2}'.format(prefix, value))
             if datapaths and k in datapaths:
                 fn = os.path.join(datapaths[k], fn)
             if k not in  res:
@@ -286,6 +290,27 @@ def _best_type(value):
         except ValueError:
             pass
     return {'true':True, 'false':False}.get(value, value)
+
+def list(source, datapaths = None):
+    '''Get a list of all `ProcessingInfo` objects in the FITS header. The
+    list is sorted by the execution order.
+
+    :param source: Object pointing to the result file header
+    :type source: :class:`str` or :class:`astropy.io.fits.HDUList`
+                  or :class:`astropy.io.fits.PrimaryHDU` or
+                  :class:`astropy.io.fits.Header`
+    :param datapaths: Dictionary with frame tags as keys and directory paths
+                      as values to provide a full path for the raw and
+                      calibration frames. Optional.
+    :type datapaths: :class:`dict`
+    '''
+    pi = []
+    for i in range(1, 2**16):
+        try:
+            pi.append(ProcessingInfo(source, i, datapaths))
+        except KeyError:
+            break
+    return pi
 
 if __name__ == '__main__':
     import sys
